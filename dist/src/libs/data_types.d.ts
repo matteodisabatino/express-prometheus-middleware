57418629ede94e6faa0f139614847c49231f8bb3
Matteo Di Sabatino <matteo.di.sabatino.1989@gmail.com>
2023-11-09 00:10:57 +0100

    feat: support prom-client v15

--- dist/src/libs/data_types.d.ts
+++ dist/src/libs/data_types.d.ts
@@ -2,10 +2,10 @@
 import { Array, Boolean, Function, Partial, Record, String } from 'runtypes';
 import express from 'express';
 import Prometheus from 'prom-client';
-export declare const PromClientRegistry: import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.Registry, unknown>;
-export declare const PromClientDefaultMetricsCollectorConfiguration: import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration, unknown>;
+export declare const PromClientRegistry: import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.Registry<"text/plain; version=0.0.4; charset=utf-8">, unknown>;
+export declare const PromClientDefaultMetricsCollectorConfiguration: import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration<Prometheus.RegistryContentType>, unknown>;
 export declare const CompleteOptions: Record<{
-    collectDefaultMetrics: import("runtypes").Union<[Boolean, import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration, unknown>]>;
+    collectDefaultMetrics: import("runtypes").Union<[Boolean, import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration<Prometheus.RegistryContentType>, unknown>]>;
     collectGCMetrics: Boolean;
     exclude: Function;
     excludePaths: Array<String, false>;
@@ -13,7 +13,7 @@ export declare const CompleteOptions: Record<{
 }, false>;
 export declare const ExpressRequest: import("runtypes").Constraint<import("runtypes").Unknown, express.Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, globalThis.Record<string, any>>, unknown>;
 export declare const Options: Partial<{
-    collectDefaultMetrics: import("runtypes").Union<[Boolean, import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration, unknown>]>;
+    collectDefaultMetrics: import("runtypes").Union<[Boolean, import("runtypes").Constraint<import("runtypes").Unknown, Prometheus.DefaultMetricsCollectorConfiguration<Prometheus.RegistryContentType>, unknown>]>;
     collectGCMetrics: Boolean;
     exclude: Function;
     excludePaths: Array<String, false>;
